6f2a89654a3c62dd8ab5715c3dba3ff32311e31c
MihailRascalovm <MihailRascalov@protonmail.com>
2020-03-08 20:15:46 +0100

    done

---
+++ zadanie_5.py
@@ -0,0 +1,10 @@
+#Zapisz swoje imię i nazwisko w oddzielnych zmiennych wszystkie
+#wielkimi literami. Użyj odpowiedniej metody by wyświetlić je
+#pisane tak, że pierwsza litera jest wielka a pozostałe małe.
+#(trzeba użyć metody capitalize)
+
+name = "MIHAIL"
+last_name = "RASCALOV"
+
+print("name = " + name.capitalize() + "\n" + "last_name = " 
+     + last_name.capitalize())
