5b796ed5d44ff0347325ea4dadcd0a9cb347b6d7
MihailRascalovm <MihailRascalov@protonmail.com>
2020-03-08 04:00:19 +0100

    zrobione

---
+++ 1_wprowadzenie/zadanie_1.py
@@ -0,0 +1,19 @@
+#Napisz pierwszy skrypt, w którym zadeklarujesz po dwie zmienne
+#każdego typu, a następnie wyświetl te zmienne.
+
+name = "Alice"
+last_name = "Bogdanov"
+
+age = 18
+bank_account_value = 0
+
+velocity = 165.78
+weight = 77.31
+
+number_one = 1.5+0.5j
+number_two = 5.5+1.5j
+
+print("Zmienne typu string = "+ name + ", " + last_name)
+print("Zmienne typu int = " + str(age) + ", " + str(bank_account_value))
+print("Zmienne typu float = "+ str(velocity) + ", " + str(weight))
+print("Zmienne typu complex = "+ str(number_one) + ", " + str(number_two))
