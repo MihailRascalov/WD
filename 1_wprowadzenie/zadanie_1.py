8ead7343dbbd83a8af28ca6cf5c2bfb226ba4e3b
MihailRascalovm <MihailRascalov@protonmail.com>
2020-03-08 04:09:37 +0100

    done

--- 1_wprowadzenie/zadanie_1.py
+++ 1_wprowadzenie/zadanie_1.py
@@ -13,7 +13,7 @@ weight = 77.31
 number_one = 1.5+0.5j
 number_two = 5.5+1.5j
 
-print("Zmienne typu string = "+ name + ", " + last_name)
-print("Zmienne typu int = " + str(age) + ", " + str(bank_account_value))
-print("Zmienne typu float = "+ str(velocity) + ", " + str(weight))
-print("Zmienne typu complex = "+ str(number_one) + ", " + str(number_two))
+print("String data variables = "+ name + ", " + last_name)
+print("Int data variables = " + str(age) + ", " + str(bank_account_value))
+print("Float data variables = "+ str(velocity) + ", " + str(weight))
+print("Complex data variables = "+ str(number_one) + ", " + str(number_two))
