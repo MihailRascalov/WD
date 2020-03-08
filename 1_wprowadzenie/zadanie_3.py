31092260ddefbe4a97cfbf2450e83afadf0932d9
MihailRascalovm <MihailRascalov@protonmail.com>
2020-03-08 05:05:00 +0100

    done

--- 1_wprowadzenie/zadanie_3.py
+++ 1_wprowadzenie/zadanie_3.py
@@ -1,17 +1,30 @@
 #Napisz skrypt, w którym stworzysz operatory przyrostkowe dla
 #operacji: +,-,*,/,**,%
 
-number = 10
-print("Initial number = " + str(number))
-number += 1
-print("Result = " + str(number))
-number -= 2
-print("Result = " + str(number))
-number *= 3
-print("Result = " + str(number))
-number /= 4
-print("Result = " + str(number))
-number **= 5
-print("Result = " + str(round(number, 2)))
-number %= 6
-print("Result = " + str(round(number, 2)))
+number_one = 10
+number_two = 5
+print("Initial number = " + str(number_one))
+
+print(str(number_one) + " += " + str(number_two))
+number_one += number_two
+print("=> " + str(number_one))
+
+print(str(number_one) + " -= " + str(number_two))
+number_one -= number_two
+print("=> " + str(number_one))
+
+print(str(number_one) + " *= " + str(number_two))
+number_one *= number_two
+print("=> " + str(number_one))
+
+print(str(number_one) + " /= " + str(number_two))
+number_one /= number_two
+print("=> " + str(number_one))
+
+print(str(number_one) + " **= " + str(round(number_two, 2)))
+number_one **= number_two
+print("=> " + str(number_one))
+
+print(str(number_one) + " %= " + str(round(number_two, 2)))
+number_one %= number_two
+print("=> " + str(number_one))
